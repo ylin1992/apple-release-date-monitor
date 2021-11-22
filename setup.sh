a06cedd8894e3885fcdf98960fcc9419a06fada4
Y Lin <linyeah2011@gmail.com>
2021-11-22 23:46:54 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-export SENDER_EMAIL='SENDER_EMAIL'
-export SENDER_PWD='YOUR PWD'
-export RECEIVER_EMAIL='RECEIVER_EMAIL'
+export SENDER_EMAIL='<SENDER_EMAIL>'
+export SENDER_PWD='<YOUR_PWD>'
+export RECEIVER_EMAIL='<RECEIVER_EMAIL>'
+export SENDGRID_API_KEY='<SENGRID_API_KEY>'
+export CHROMEDRIVER_PATH='/app/.chromedriver/bin/chromedriver'
+export GOOGLE_CHROME_BIN='/app/.apt/usr/bin/google-chrome'
