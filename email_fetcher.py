81c1bab1874908d17cbdd43b2e3859289627987d
Yeh Lin <linyeah2011@gmail.com>
2021-11-30 22:43:30 +0800

    exceptio

--- email_fetcher.py
+++ email_fetcher.py
@@ -2,7 +2,10 @@ import requests
 import os
 from config import DB_SERVER_HOST
 def get_all_users():
-    res = requests.get(DB_SERVER_HOST+'/users?subscribed=true')
+    try:
+        res = requests.get(DB_SERVER_HOST+'/users?subscribed=true')
+    except Exception as e:
+        print(e)
     if res.status_code == 200:
         users = res.json().get('users')
         emails = [u['email'] for u in users]
